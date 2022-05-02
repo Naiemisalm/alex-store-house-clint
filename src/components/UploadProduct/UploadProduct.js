6e7cefd5cd2cfad73ae2562a6b1dd935b52dc7a3
Naiemisalm <nayemislam30964@gmail.com>
2022-05-02 10:44:32 +0600

    product added

--- src/components/UploadProduct/UploadProduct.js
+++ src/components/UploadProduct/UploadProduct.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
 
 const UploadProduct = () => {
+
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+        const url = (`http://localhost:5000/product`);
+        fetch(url, {
+            method: 'POST',
+            headers:{
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res=> res.json())
+        .then(result=>{
+            console.log(result);
+        })
+
+    }
+
+
     return (
+        
         <div>
-            <h1>UploadProduct</h1>
+            <h2 className='text-center'>Upload porduct</h2>
+            
+            <div className='w-50 mx-auto mt-5'>
+
+                <form className=' d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
+                    <input className='mb-3' placeholder='Name' {...register("name", { required: true, maxLength: 20 })} />
+                    <textarea className='mb-3' placeholder='Discription' {...register("discription", )} />
+                    <input className='mb-3' placeholder='Photo Url' type ="text" {...register("img")} />
+                    <input className='mb-3' placeholder='Price' type="number" {...register("price")} />
+                    <input className='mb-3' placeholder='Quantity' type="number" {...register("quantity")} />
+                    <input  type="submit" value="Upload Product" />
+                </form>
+            </div>
         </div>
+
     );
 };
 
